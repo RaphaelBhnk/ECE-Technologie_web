09efcfcba4d24b85f5eb3a3424b592d59b7e5374
RaphaelBhnk <raphael.bouhnik@edu.ece.fr>
2020-11-18 00:17:10 +0100

    Question3

--- front-end/src/Channels.js
+++ front-end/src/Channels.js
@@ -1,13 +1,37 @@
+
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 import {useState, useEffect} from 'react';
 import axios from 'axios';
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
 // Layout
 import Link from '@material-ui/core/Link'
+import { useMediaPredicate } from "react-media-hook";
+import { Drawer } from '@material-ui/core';
+const useStyles = makeStyles({
+  list: {
+    width: '100%',
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
+
 
 const styles = {
   root: {
-    minWidth: '200px',
+    minWidth: '100px',
   },
   channel: {
     padding: '.2rem .5rem',
@@ -18,7 +42,6 @@ const styles = {
 export default ({
   onChannel
 }) => {
-  const [channels, setChannels] = useState([])
   useEffect( () => {
     const fetch = async () => {
       const {data: channels} = await axios.get('http://localhost:3001/channels')
@@ -26,8 +49,58 @@ export default ({
     }
     fetch()
   }, [])
-  return (
-    <ul style={styles.root}>
+
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {channels.map( (channel, i) => (
+        <li key={i}>
+          <Link
+            href="#"
+            onClick={ (e) => {
+              e.preventDefault()
+              onChannel(channel)
+            }}
+            >
+            {channel.name}
+          </Link>
+        </li>
+      ))}
+      </List>
+     
+ 
+    </div>);
+
+
+  const smallerThan400 = useMediaPredicate("(max-width: 400px)");
+  const biggerThan400 = useMediaPredicate("(min-width: 401px)");
+  const [channels, setChannels] = useState([])
+
+  return (<React.Fragment>
+    {biggerThan400 && <ul style={styles.root}>
       { channels.map( (channel, i) => (
         <li key={i} css={styles.channel}>
           <Link
@@ -41,6 +114,25 @@ export default ({
           </Link>
         </li>
       ))}
-    </ul>
+    </ul>}
+    {smallerThan400 &&     <div>
+      {['Channels'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <SwipeableDrawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+            onOpen={toggleDrawer(anchor, true)}
+          >
+            {list(anchor)}
+          </SwipeableDrawer>
+        </React.Fragment>
+      ))}
+    </div>
+  }
+    </React.Fragment>
   );
 }
+
+
