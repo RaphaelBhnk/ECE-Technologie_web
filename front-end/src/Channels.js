9a9b877100e080543eee2bcd20a76b90cf4418b6
RaphaelBhnk <raphael.bouhnik@edu.ece.fr>
2020-12-07 19:04:49 +0100

    Création de channel ok

--- front-end/src/Channels.js
+++ front-end/src/Channels.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { jsx } from '@emotion/core'
 // Layout
 import Link from '@material-ui/core/Link'
+import Button from '@material-ui/core/Button'
 // Local
 import Context from './Context'
 import {useHistory} from 'react-router-dom'
@@ -15,7 +16,21 @@ const styles = {
   channel: {
     padding: '.2rem .5rem',
     whiteSpace: 'nowrap', 
-  }
+  },
+  bou:{
+    width:'100%',
+    height:'30px',
+    border: '4px double #cccccc',
+    borderRadius: '10px',
+    fontSize: '15px',
+    backgroundColor:'#D3816E',
+    color:'white',
+    ':hover': {
+      backgroundColor: 'rgba(255,255,255,.05)',
+    },
+    transition: '0.5s'
+  },
+
 }
 
 export default () => {
@@ -23,6 +38,18 @@ export default () => {
     oauth,
     channels, setChannels
   } = useContext(Context)
+  const createChannel = async () => {
+    var nameChannel=prompt("Please enter the name of the channel:")
+    const {data: channel} = await axios.post(
+      `http://localhost:3001/channels`
+    , {
+      name: nameChannel,
+      propriétaire: oauth.email,
+      membres:[]
+    })
+    window.location.reload("http://localhost:3000/channels");
+  }
+
   const history = useHistory();
   useEffect( () => {
     const fetch = async () => {
@@ -32,6 +59,7 @@ export default () => {
             'Authorization': `Bearer ${oauth.access_token}`
           }
         })
+
         setChannels(channels)
       }catch(err){
         console.error(err)
@@ -50,10 +78,19 @@ export default () => {
               history.push(`/channels/${channel.id}`)
             }}
           >
-            {channel.name}
+          {
+            channel.propriétaire===oauth.email ? <center><button css={styles.bou}>{channel.name}</button> </center>   
+             : channel.membres.indexOf(oauth.email)!==-1 ? (<center><button css={styles.bou}>{channel.name}</button> </center>)
+             :<center></center>
+             } 
           </Link>
         </li>
       ))}
+    <center><Button variant="contained" color="primary" disableElevation  onClick={createChannel}>
+    Add channel
+  </Button>
+  </center>
     </ul>
+
   );
 }
