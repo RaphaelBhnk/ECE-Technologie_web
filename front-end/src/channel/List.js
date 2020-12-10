3f8dbf7061bf0aa591774d697fbff6216927151b
RaphaelBhnk <raphael.bouhnik@edu.ece.fr>
2020-12-10 01:42:53 +0100

    Add members/administrators to channel & rename channel & popup

--- front-end/src/channel/List.js
+++ front-end/src/channel/List.js
@@ -1,13 +1,21 @@
 import {forwardRef,useEffect, useImperativeHandle, useLayoutEffect, useRef,useContext} from 'react'
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
+import '../App.css';
 import Context from '../Context'
 import Button from '@material-ui/core/Button'
 import axios from 'axios';
 import { useHistory } from 'react-router-dom'
-
+//Menu
+import React from 'react';
+import { TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 // Layout
 import { useTheme } from '@material-ui/core/styles';
+//Icon
+import MoreHorizOutlinedIcon from '@material-ui/icons/MoreHorizOutlined';
 // Markdown
 import unified from 'unified'
 import markdown from 'remark-parse'
@@ -24,6 +32,7 @@ dayjs.updateLocale('en', {
     sameElse: 'DD/MM/YYYY hh:mm A'
   }
 })
+
 
 const useStyles = (theme) => ({
   root: {
@@ -57,6 +66,18 @@ const useStyles = (theme) => ({
     top: 0,
     width: '50px',
   },
+  left:{
+    float:'left',
+    margin:'50'
+  },
+  corner:{
+    float:'right',
+  },
+  big:{
+    margin:'20px',
+    transform: 'scale(2)'
+  }
+
 })
 
 export default forwardRef(({
@@ -64,6 +85,18 @@ export default forwardRef(({
   messages,
   onScrollDown,
 }, ref) => {
+  
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const classes = useStyles();
+
   const styles = useStyles(useTheme())
   // Expose the scroll action
   useImperativeHandle(ref, () => ({
@@ -78,21 +111,77 @@ export default forwardRef(({
   const scrollEl = useRef(null)
   const history = useHistory()
 
- const addMember = async()=>{
+ const rename = async()=>{
     var t= channel
-    var k=prompt("Ajoutez un membre:")
-    t.name=k
+    var renameChannel=document.getElementById("filled-basic rename").value
+    if(renameChannel!==null){t.name=renameChannel}
     try {
       
-      const {data: channels} = axios.put(
+      const {data: channels} = await axios.put(
         `//localhost:3001/channels/${t.id}`
       , {
         name:t.name,
-        propriétaire:t.propriétaire,
-        membres:t.membres
+        owner:t.owner,
+        members:t.members,
+        administrators:t.administrators
       })
-
-      history.push(`/channels`)
+      handleClose()
+      history.push(`/channels/${t.id}`)
+      
+    } catch (error) {
+    }
+  }
+  const addMember = async()=>{
+    var t= channel
+    var newMember=document.getElementById("filled-basic rename").value
+    if(newMember!==null){t.members.push(newMember)}
+    try {
+      
+      const {data: channels} = await axios.put(
+        `//localhost:3001/channels/${t.id}`
+      , {
+        name:t.name,
+        owner:t.owner,
+        members:t.members,
+        administrators:t.administrators
+      })
+      handleClose()
+      history.push(`/channels/${t.id}`)
+      
+    } catch (error) {
+    }
+  }
+  const makeAdmin = async()=>{
+    var t= channel
+    console.log(t.administrators)
+    var k=prompt("Add the email of the new member:","")
+    if(k!==null){t.administrators.push(k)}
+    try {
+      
+      const {data: channels} = await axios.put(
+        `//localhost:3001/channels/${t.id}`
+      , {
+        name:t.name,
+        owner:t.owner,
+        members:t.members,
+        administrators:t.administrators
+      })
+      handleClose()
+      history.push(`/channels/${t.id}`)
+      
+    } catch (error) {
+    }
+  }
+  const deleteChannel = async()=>{
+    var t= channel
+    try {
+      
+      const {data: channels} = await axios.delete(
+        `//localhost:3001/channels/${t.id}`,
+        t.id
+      )
+      handleClose()
+      history.push(`/channels/`)
       
     } catch (error) {
     }
@@ -120,13 +209,47 @@ export default forwardRef(({
 
   return (
     <div css={styles.root} ref={rootEl}>
-      <h1>Messages for {channel.name}</h1>
-      {
-      channel.propriétaire===oauth.email ? <Button variant="contained" color="primary" onClick={addMember} >
-      Add a member
-    </Button>
+      <div>
+      <h1 css={styles.left}>Messages for {channel.name}</h1>
+      {channel.owner===oauth.email ? <span css={styles.corner}><Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+    <MoreHorizOutlinedIcon css={styles.big}></MoreHorizOutlinedIcon>
+  </Button>
+        <Menu
+    id="simple-menu"
+    anchorEl={anchorEl}
+    keepMounted
+    open={Boolean(anchorEl)}
+    onClose={handleClose}
+  >
+    <a class="effectless"  href="#popupRename"><MenuItem>Rename channel</MenuItem></a>
+    <a class="effectless" href="#popupAdd"><MenuItem>Add member</MenuItem></a>
+    <a class="effectless" href="#popupDel"><MenuItem>Delete this channel</MenuItem></a>
+    <a class="effectless" href="#popupAdmin"><MenuItem>Make someone administrator</MenuItem></a>
+  </Menu></span>
+  
+    //    <Button variant="contained" color="primary" onClick={addMember} >
+    //   Rename channel
+    // </Button>
+     : channel.administrators.indexOf(oauth.email)!==-1 ? <span css={styles.corner}><Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+     <MoreHorizOutlinedIcon css={styles.big}></MoreHorizOutlinedIcon>
+   </Button>
+         <Menu
+     id="simple-menu"
+     anchorEl={anchorEl}
+     keepMounted
+     open={Boolean(anchorEl)}
+     onClose={handleClose}
+   >
+    <a class="effectless" href="#popupAdd"><MenuItem>Add member</MenuItem></a>
+    <a class="effectless" href="#popupAdmin"><MenuItem>Make someone administrator</MenuItem></a>
+   </Menu></span>
      : <p></p>
       }
+     </div>
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
       <ul>
         { messages.map( (message, i) => {
             const {contents: content} = unified()
@@ -148,6 +271,61 @@ export default forwardRef(({
         })}
       </ul>
       <div ref={scrollEl} />
+      <div id="popupRename" class="overlay">
+             <div class="popup">
+               <h2>Rename your channel:</h2>
+               <a class="close" href="#">&times;</a>
+               <div class="content">
+               <form className={classes.root} noValidate autoComplete="off">
+               <TextField id="filled-basic rename" label="Channel name" variant="filled" />
+               <div className={classes.root}>
+               <Button onClick={rename} href="#">RENAME</Button>
+                </div>
+                </form>
+               </div>
+             </div>
+           </div>
+           <div id="popupAdd" class="overlay">
+             <div class="popup">
+               <h2>Add a new member:</h2>
+               <a class="close" href="#">&times;</a>
+               <div class="content">
+               <form className={classes.root} noValidate autoComplete="off">
+               <TextField id="filled-basic add" label="Member name" variant="filled" />
+               <div className={classes.root}>
+               <Button onClick={addMember} href="#">ADD</Button>
+                </div>
+                </form>
+               </div>
+             </div>
+           </div>
+           <div id="popupDel" class="overlay">
+             <div class="popup">
+               <h2>You really want to delete this channel ?</h2>
+               <a class="close" href="#">&times;</a>
+               <div class="content">
+               <form className={classes.root} noValidate autoComplete="off">
+               <div className={classes.root}>
+               <Button onClick={deleteChannel} href="#">DELETE</Button>
+                </div>
+                </form>
+               </div>
+             </div>
+           </div>
+           <div id="popupAdmin" class="overlay">
+             <div class="popup">
+               <h2>Make someone administrator:</h2>
+               <a class="close" href="#">&times;</a>
+               <div class="content">
+               <form className={classes.root} noValidate autoComplete="off">
+               <TextField id="filled-basic add" label="Administrator name" variant="filled" />
+               <div className={classes.root}>
+               <Button onClick={makeAdmin} href="#">ADD</Button>
+                </div>
+                </form>
+               </div>
+             </div>
+           </div>
     </div>
   )
 })
